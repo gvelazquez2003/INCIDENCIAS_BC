cde2edba4bebc37b81365a837b524be2cb593fe1
estebancorr <correa.esteban@correo.unimet.edu.ve>
2026-05-29 14:36:11 -0400

    Nuevo URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzexubb_QCiR45l0nkVhxm5B31KFk3gwLoYS4X2Vz9KrGpMv_I9ksshJKvNcOuUd2gKJA/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyWjV0ky8iI_WNd7f9V4vyO5GvXqVs8vhGirA3EFLSkwkrfk6ezF5g-6VRpDdgF_fZ0/exec?action=setupPrices';
